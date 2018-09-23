f87b90a5c3e03c1246bd71266b58c6867b0fc457
Joseph'5 <freelife.dn@gmail.com>
2018-09-23 19:19:07 +0700

    upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1537678462,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bgmenu-sheet0.png",
+		"images/bgnplay-sheet0.png",
+		"images/bgover-sheet0.png",
+		"images/word.png",
+		"images/coffee-sheet0.png",
+		"images/coffee-sheet1.png",
+		"media/correct.m4a",
+		"media/correct.ogg",
+		"media/lose.m4a",
+		"media/lose.ogg",
+		"media/gavin-d-x-d-gerrard.m4a",
+		"media/gavin-d-x-d-gerrard.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
